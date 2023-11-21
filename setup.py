2f5c593df914f8809fab93f2632e81a4c673a8bb
Cyberwolf098 <95936532+Cyberwolf098@users.noreply.github.com>
2023-11-21 12:41:24 -0500

    upload file

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools twine
+setuptools.setup(
+ name='Encryptor_class',
+ version='0.0.1',
+ author="Cyberwolf&Kaln509",
+ author_email="ralphsaintil0@gmail.com",
+ description="c'est un algoritme de cryptage oriente objet",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
